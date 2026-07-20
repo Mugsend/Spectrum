70a3190feff09c32baaf65f3887514944b2a7df8
mugsend <chaudharysaurabh93063@gmail.com>
2026-07-20 13:50:37 +0530

    Revert "add grid lines for frequencies and decibels, smooth out the display magnitude"
    This reverts commit a731223e0ddc394c3cd62892635c2856abc4967c.

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,29 +13,41 @@
 SpectrumAudioProcessorEditor::SpectrumAudioProcessorEditor (SpectrumAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {   
-    binSizeMenu.addItem("2048", 1);
-    binSizeMenu.addItem("4096", 2);
-    binSizeMenu.addItem("8192", 3);
-    binSizeMenu.addItem("16384", 4);
+    
+    addAndMakeVisible(meter);
+    addAndMakeVisible(binSizeMenu);
 
-    binSizeMenu.setSelectedId(1);
-
+    binSizeMenu.addItem("128 Bins", 1);
+    binSizeMenu.addItem("256 Bins", 2);
+    binSizeMenu.addItem("512 Bins", 3);
+    binSizeMenu.addItem("1024 Bins", 4);
+    binSizeMenu.addItem("2048 Bins", 5);
+    binSizeMenu.addItem("4096 Bins", 6);
+    binSizeMenu.addItem("8192 Bins", 7);
+     
     binSizeMenu.onChange = [this]()
         {
             int selectedId = binSizeMenu.getSelectedId();
 
+            int newFftSize = 512;
+
             switch (selectedId)
             {
-            case 1: audioProcessor.currentFftOrder.store(11); break;
-            case 2: audioProcessor.currentFftOrder.store(12); break;
-            case 3: audioProcessor.currentFftOrder.store(13); break;
-            case 4: audioProcessor.currentFftOrder.store(14); break;
+            case 1: newFftSize = 256;  break;
+            case 2: newFftSize = 512;  break;
+            case 3: newFftSize = 1024; break;
+            case 4: newFftSize = 2048; break;
+            case 5: newFftSize = 4096; break;
+            case 6: newFftSize = 8192; break;
+            case 7: newFftSize = 16384; break;
             }
+
+            audioProcessor.currentFftSize.store(newFftSize);
         };
 
-    addAndMakeVisible(meter);
-    addAndMakeVisible(binSizeMenu);
-    setSize(1200,300);
+    binSizeMenu.setSelectedId(2);
+
+    setSize(1000,300);
 }
 
 SpectrumAudioProcessorEditor::~SpectrumAudioProcessorEditor()
@@ -50,10 +62,11 @@ void SpectrumAudioProcessorEditor::paint (juce::Graphics& g)
 
 void SpectrumAudioProcessorEditor::resized()
 {
-    auto bounds = getLocalBounds();
+    auto bounds = getLocalBounds().reduced(10);
 
-    binSizeMenu.setBounds(45,8,120,24);
+    binSizeMenu.setBounds(bounds.removeFromTop(30));
 
+    bounds.removeFromTop(10);
 
     meter.setBounds(bounds);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -14,9 +14,9 @@
 class MagnitudeMeter : public juce::Component, public juce::Timer
 {
 public:
-    MagnitudeMeter(SpectrumAudioProcessor& p) : processor(p)
+    // Pass in a reference to your atomic magnitude variable
+    MagnitudeMeter(std::array<std::atomic<float>,SpectrumAudioProcessor::maxBins>& magValues, std::atomic<int>& activeBins) : magnitudeValues(magValues), activeNumBins(activeBins)
     {
-        std::fill(smoothedData.begin(), smoothedData.end(), -120.0f);
         startTimerHz(60);
     }
 
@@ -24,146 +24,50 @@ public:
     {
         stopTimer();
     }
-    
+
     void timerCallback() override
-    {        
-        int order = processor.currentFftOrder.load();
-        int size = 1 << order;
-        if (processor.nextFFTBlockReady.load())
-        {
-            if (order == 11)
-            {
-                processor.window2048.multiplyWithWindowingTable(processor.fftData.data(), size);
-                processor.fft2048.performFrequencyOnlyForwardTransform(processor.fftData.data());
-            }
-            else if (order == 12)
-            {
-                processor.window4096.multiplyWithWindowingTable(processor.fftData.data(), size);
-                processor.fft4096.performFrequencyOnlyForwardTransform(processor.fftData.data());
-            }
-            else if (order == 13)
-            {
-                processor.window8192.multiplyWithWindowingTable(processor.fftData.data(), size);
-                processor.fft8192.performFrequencyOnlyForwardTransform(processor.fftData.data());
-            }
-            else if (order == 14)
-            {
-                processor.window16384.multiplyWithWindowingTable(processor.fftData.data(), size);
-                processor.fft16384.performFrequencyOnlyForwardTransform(processor.fftData.data());
-            }
-
-            processor.nextFFTBlockReady.store(false);
-
-            
-        }
-
-        int binsToDraw = size / 2;
-
-        const float decayFactor = 0.85f;
-
-        for (int i = 0; i < binsToDraw; ++i)
-        {
-            
-            float rawMagnitude = processor.fftData[i];
-            float targetDb = juce::Decibels::gainToDecibels(rawMagnitude) - juce::Decibels::gainToDecibels(static_cast<float>(size));
-
-            
-            targetDb = juce::jmax(targetDb, -100.0f);
-
-            
-            smoothedData[i] = (decayFactor * smoothedData[i]) + ((1.0f - decayFactor) * targetDb);
-        }
+    {
+        
         repaint();
     }
 
     void paint(juce::Graphics& g) override
     {
+        g.fillAll(juce::Colours::black);
 
-        juce::Path spectrumPath;
-
-        int order = processor.currentFftOrder.load();
-        int activeFftSize = 1 << order;
-        int binsToDraw = activeFftSize / 2;
+        int binsToDraw = activeNumBins.load();
         if (binsToDraw == 0) return;
-
-        float minLog = std::log(1.0f);
-        float maxLog = std::log(static_cast<float>(binsToDraw - 1));
 
         auto width = static_cast<float>(getWidth());
         auto height = static_cast<float>(getHeight());
 
         float binWidth = width / static_cast<float>(binsToDraw);
         
-        const float minDb = -120.0f;
+        const float minDb = -100.0f;
         const float maxDb = 0.0f;
-
-        for (float db = minDb;db < maxDb;db += 12) 
-        {
-            float yPos = juce::jmap(db, -120.0f, 0.0f, height, 0.0f);
-            float snapped_Y = static_cast<float>(juce::roundToInt(yPos));
-
-            g.drawLine(0, snapped_Y, width, snapped_Y);
-            g.setColour(juce::Colours::darkgrey);
-            g.drawText(juce::String(db) + "db", 5, yPos - 12, 50, 10, juce::Justification::left);
-            g.setColour(juce::Colours::lightgrey.withAlpha(0.1f));
-        }
-        float sampleRate = static_cast<float>(processor.getSampleRate());
-        std::array<float, 10> displayFreqs = { 20.0f, 50.0f, 100.0f, 200.0f, 500.0f, 1000.0f, 2000.0f, 5000.0f, 10000.0f, 20000.0f };
-
-        if (sampleRate>0.0f)
-        {
-            for (float freq : displayFreqs)
-            {
-                float binIndex = freq * (static_cast<float>(activeFftSize) / sampleRate);
-                float currentLog = std::log(binIndex);
-                float normalisedX = (currentLog - minLog) / (maxLog - minLog);
-                float xPos = normalisedX * width;
-                float snapped_X = static_cast<float>(juce::roundToInt(xPos));
-                
-                g.drawLine(snapped_X, height, snapped_X, 0);
-                g.setColour(juce::Colours::darkgrey);
-                juce::String freqText = freq >= 1000.0f ? juce::String(freq / 1000.0f) + "k" : juce::String(freq);
-                g.drawText(freqText, snapped_X + 4, static_cast<int>(height) - 15, 30, 10, juce::Justification::left);
-                g.setColour(juce::Colours::lightgrey.withAlpha(0.1f));
-
-
-            }
-        }
-        
         const float dbRange = maxDb - minDb;
 
         g.setColour(juce::Colours::blue);
 
-        for (int i = 1; i < binsToDraw; ++i) 
-        {
-            float currentLog = std::log(static_cast<float>(i));
-            float normalisedX = (currentLog - minLog) / (maxLog - minLog);
-            float xPos = normalisedX * width;
-            float snapped_X = static_cast<float>(juce::roundToInt(xPos));
-
-            float currentDb = smoothedData[i];
-            float yPos = juce::jmap(currentDb, -120.0f, 0.0f, height, 0.0f);
+        for (int i = 0; i < binsToDraw; ++i) {
+            float currentDb = magnitudeValues[i].load();
 
             float normalisedDb = (currentDb - minDb) / dbRange;
 
             float mappedY = height - (normalisedDb * height);
-            
-            if (i == 1)
-            {
-                spectrumPath.startNewSubPath(xPos, yPos);
-            }
-            else
-            {
-                spectrumPath.lineTo(xPos, yPos);
-            }
+            float xPos = i * binWidth;
+            float barHeight = height - mappedY;
+
+            //g.fillRect(xPos, mappedY, binWidth - 1.0f, barHeight);
+            g.drawLine(xPos, height, xPos, barHeight);
                 
         }
-        g.strokePath(spectrumPath, juce::PathStrokeType(0.5f));
     }
 
 private:
-    SpectrumAudioProcessor& processor;
-    std::array<float, SpectrumAudioProcessor::maxFftSize / 2> smoothedData;
+    std::array<std::atomic<float>, SpectrumAudioProcessor::maxBins>&magnitudeValues;
+    std::atomic<int>& activeNumBins;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MagnitudeMeter)
 };
 //==============================================================================
@@ -178,12 +82,11 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
-    
 
 private:
     SpectrumAudioProcessor& audioProcessor;
 
-    MagnitudeMeter meter{ audioProcessor };
+    MagnitudeMeter meter{ audioProcessor.magnitudes, audioProcessor.activeNumBins };
 
     juce::ComboBox binSizeMenu;
 
